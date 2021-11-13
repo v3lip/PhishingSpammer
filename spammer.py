59d2c87aac43231ea25561d160494a54912a7da7
velip <anders_teehee@hotmail.no>
2021-11-13 02:20:08 +0100

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -51,17 +51,8 @@ url = settings['targetUrl']
 
 #Makes terminal sexy
 COLORS = {\
-"black":"\u001b[30;1m",
 "red": "\u001b[31;1m",
-"green":"\u001b[32m",
-"yellow":"\u001b[33;1m",
-"blue":"\u001b[34;1m",
 "magenta":"\u001b[35m",
-"cyan": "\u001b[36m",
-"white":"\u001b[37m",
-"yellow-background":"\u001b[43m",
-"black-background":"\u001b[40m",
-"cyan-background":"\u001b[46;1m",
 "reset":"\u001b[0m",
 }
 
